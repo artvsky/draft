71b9fcc58db2773d9862fe3de3310ab9846f4d8e
eremeev-2001 <eremeev-2001@yandex.ru>
2019-09-24 12:08:27 +0300

    Тест

--- Home2D.py
+++ Home2D.py
@@ -1,9 +1,18 @@
 from graphics import *
 
-win = GraphWin("Home2D", 640, 360)
+win = GraphWin("Home2D", 1000, 1000)
+
+def paint all
+
+
 
 c1 = Circle(Point(568, 68), 32)
 c1.setFill("#ffff00")
+
+
+
+
+
 r1 = Rectangle(Point(247, 200), Point(421, 336))
 r1.setFill("#804000")
 r2 = Rectangle(Point(268, 218), Point(329, 249))
@@ -31,6 +40,38 @@ l5 = Line(Point(308, 159), Point(308, 187))
 l6 = Line(Point(343, 173), Point(371, 173))
 
 
+
+r11 =Rectangle(Point(100, 100), Point(187, 168))
+r11.setFill("#804000")
+r21 = Rectangle(Point(110.5, 109), Point(141,124.5))
+r21.setFill("#0080ff")
+r31 = Rectangle(Point(156, 109), Point(177.5,118.5 ))
+r31.setFill("#0080ff")
+r41 = Rectangle(Point(268, 268), Point(330, 316))
+r41.setFill("#0080ff")
+l11 = Line(Point(299, 268), Point(299, 316))
+l21 = Line(Point(299, 281), Point(330, 281))
+r51 = Rectangle(Point(365, 268), Point(397, 336))
+r51.setFill("#ff8040")
+c21 = Circle(Point(392, 303), 2)
+c21.setFill("#008000")
+p11 = Polygon(Point(213, 200), Point(331, 101), Point(454, 200))
+p11.setFill("#008080")
+p21 = Polygon(Point(247, 200), Point(331, 101), Point(420, 200))
+c31 = Circle(Point(308, 173), 14)
+c31.setFill("#00ffff")
+c41= Circle(Point(357, 173), 14)
+c41.setFill("#00ffff")
+l31 = Line(Point(294, 173), Point(322, 173))
+l41 = Line(Point(357, 159), Point(357, 187))
+l51 = Line(Point(308, 159), Point(308, 187))
+l61= Line(Point(343, 173), Point(371, 173))
+
+
+
+
+
+
 c1.draw(win)
 r1.draw(win)
 r2.draw(win)
@@ -48,6 +89,24 @@ l3.draw(win)
 l4.draw(win)
 l5.draw(win)
 l6.draw(win)
+
+
+r11.draw(win)
+r21.draw(win)
+r31.draw(win)
+r41.draw(win)
+l11.draw(win)
+l21.draw(win)
+r51.draw(win)
+c21.draw(win)
+p11.draw(win)
+p21.draw(win)
+c31.draw(win)
+c41.draw(win)
+l31.draw(win)
+l41.draw(win)
+l51.draw(win)
+l61.draw(win)
 
 win.getMouse()
 win.close()
